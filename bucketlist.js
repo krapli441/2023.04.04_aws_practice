8946a923ddda1f5c62a4d7e7524444f255d8538a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-04 17:43:24 +0900

    quarantineV2 제거하고 커밋

--- bucketlist.js
+++ bucketlist.js
@@ -5,8 +5,14 @@ AWS.config.update({ region: "ap-northeast-2" });
 
 // const access = require("./access.js");
 
+const key = require("./key.js");
+
+console.log(key.access_key_id);
+console.log(key.secret_access_key);
+
 const s3 = new AWS.S3({
-  region: "ap-northeast-2", // ex) ap-northeast-2
+  accessKeyId: key.access_key_id,
+  secretAccessKey: key.secret_access_key,
 });
 
 // Call S3 to list the buckets
--- test.js
+++
@@ -1,4 +0,0 @@
-const access = require("./access.js");
-
-console.log(access.accessKeyId);
-console.log(access.secretAccessKey);
