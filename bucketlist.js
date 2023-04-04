0ac7c4bfa1f0e7684573eab70c99a9e15ce62f1a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-04 17:27:45 +0900

    암호 삭제

--- bucketlist.js
+++ bucketlist.js
@@ -3,13 +3,10 @@ var AWS = require("aws-sdk");
 // Set the region
 AWS.config.update({ region: "ap-northeast-2" });
 
-const access = require("./access.js");
+// const access = require("./access.js");
 
 const s3 = new AWS.S3({
-  accessKeyId: access.accessKeyId,
-  secretAccessKey: access.secretAccessKey,
   region: "ap-northeast-2", // ex) ap-northeast-2
-  signatureVersion: "v4",
 });
 
 // Call S3 to list the buckets
