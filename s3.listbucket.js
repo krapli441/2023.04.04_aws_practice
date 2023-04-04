37aef6aee33929918ddae088b4e7dcb843bcdacd
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-04 14:29:39 +0900

    v2-v3로 변환해봄

--- s3.listbucket.js
+++ s3.listbucket.js
@@ -1,11 +1,12 @@
 // Load the AWS SDK for Node.js
 
 import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 // Set the region
 AWS.config.update({ region: "ap-northeast-2" });
 
 // Create S3 service object
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+const s3 = new S3({ apiVersion: "2006-03-01" });
 
 // Call S3 to list the buckets
 s3.listBuckets(function (err, data) {
