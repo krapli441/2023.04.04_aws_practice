bff0f8b37df2953e7a5f757bb0343b008f8c4fee
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-04 15:51:34 +0900

    보안을 위해 키 정보 삭제

--- bucketlist.js
+++ bucketlist.js
@@ -1,21 +1,16 @@
 // Load the AWS SDK for Node.js
 var AWS = require("aws-sdk");
-// import AWS from "aws-sdk";
-// import { S3 } from "./node_modules/aws-sdk/clients/s3.js";
-
-var S3 = require("aws-sdk/clients/s3.js");
+// Set the region
+AWS.config.update({ region: "ap-northeast-2" });
 
 const access = require("./access.js");
 
 const s3 = new AWS.S3({
   accessKeyId: access.accessKeyId,
   secretAccessKey: access.secretAccessKey,
-  apiVersion: "2006-03-01",
+  region: "ap-northeast-2", // ex) ap-northeast-2
+  signatureVersion: "v4",
 });
-// Set the region
-AWS.config.update({ region: "ap-northeast-2" });
-
-// Create S3 service object
 
 // Call S3 to list the buckets
 s3.listBuckets(function (err, data) {
